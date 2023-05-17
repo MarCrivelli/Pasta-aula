de848ec80592d32dfc73a7d100086ae31cc872a6
Luiz F. Picolo <luizpicolo@gmail.com>
2023-05-17 17:39:03 -0400

    O script foi alterado e o CSS também

--- script.js
+++ script.js
@@ -18,11 +18,21 @@ class ProdutoDestaque extends  Produto{
         this.imagem =  imagem
     }
     mostrarProdutosEmDestaque(){
-        return this.imagem + this.titulo + this.descricao2;
+        return `
+            <div class="cardProduto">
+            <h3 class="tituloDestaque">${this.nome}</h3>
+            <img class="imagensDestaque"src="${this.imagem}" />
+            </div>
+        `
+        
     }
 }
 
-const produto = new Produto("Guitarra, ", "20-02-2023, ", "Guitarra pop preta pintura brilhante ultra moderna, ", 1350.99)
-const produtoDestaque = new ProdutoDestaque("Estrela Ninja, ", "18-07-2013, ","Estrela ninja preta brilhante alcance longo fatal, ", 809.99, "https://d1qejy7034t4b2.cloudfront.net/Custom/Content/Products/04/88/estrela-shuriken-preta-11cm-de-diametro-kantas-4326b-m1.jpg ,", "ESTRELA NINJA" )
+const produto = new Produto("Guitarra", "20-02-2023", "Guitarra pop preta pintura brilhante ultra moderna", 1350.99)
+const produtoDestaque = new ProdutoDestaque("Estrela Ninja", "18-07-2013","Estrela ninja preta brilhante alcance longo fatal", 809.99, "shuriken.jpg", "ESTRELA NINJA" )
+
+const produto_destaque = document.getElementById("produto-destaque")
+produto_destaque.insertAdjacentHTML('afterbegin', produtoDestaque.mostrarProdutosEmDestaque());
+
 console.log(produto.mostrarDados())
 console.log(produtoDestaque.mostrarProdutosEmDestaque())
