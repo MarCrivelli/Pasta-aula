3ffd1f4fe33e215fce5680dc66c61f467e693855
MarCrivelli <marcoantonio7crivelli@gmail.com>
2023-05-20 22:14:09 -0300

    Atualização!
    Adicionado novos botões e erros foram concertados!

--- script.js
+++ script.js
@@ -1,38 +1,67 @@
-// divListaProduto.insertAdjacentHTML('afterbegin', '');
-
-class Produto{
-    constructor(nome, datacadastro, descricao, preco){
-        this.nome = nome
-        this.sobrenome = datacadastro
-        this.inscricao = descricao
-        this.preco = preco
+class Produto {
+    constructor(nome, datacadastro, descricao, preco) {
+      this.nome = nome;
+      this.datacadastro = datacadastro;
+      this.descricao = descricao;
+      this.preco = preco;
     }
-    mostrarDados(){
-        return this.nome + this.datacadastro + this.descricao + this.preco;
+  
+    mostrarProdutos() {
+      return this.nome + " " + this.datacadastro + " " + this.descricao + " " + this.preco;
     }
-}
-
-class ProdutoDestaque extends  Produto{
-    constructor(nome, datacadastro, descricao, preco, imagem){
-        super(nome, datacadastro, descricao, preco)
-        this.imagem =  imagem
+  }
+  
+  class ProdutoDestaque extends Produto {
+    constructor(nome, datacadastro, descricao, preco, imagem) {
+      super(nome, datacadastro, descricao, preco);
+      this.imagem = imagem;
     }
-    mostrarProdutosEmDestaque(){
-        return `
-            <div class="cardProduto">
-            <h3 class="tituloDestaque">${this.nome}</h3>
-            <img class="imagensDestaque"src="${this.imagem}" />
-            </div>
-        `
-        
+  
+    mostrarProdutosEmDestaque() {
+      return `
+        <div class="cardProduto">
+          <h3 class="tituloDestaque">${this.nome}</h3>
+          <h3 class="dataCadastro">${this.datacadastro}</h3>
+          <img class="imagensDestaque" src="${this.imagem}" />
+          <h5 class="preco">${this.preco}</h5>
+          <p class="descricao">${this.descricao}</p>
+        </div>
+      `;
     }
-}
+  }
+  
+  const produtoDestaque = new ProdutoDestaque(
+    "Estrela ninja",
+    "18-07-2013",
+    "Estrela ninja preta brilhante alcance longo fatal",
+    "R$ 809.99",
+    "shuriken.jpg"
+  );
+  
+  const produto_destaque1 = document.getElementById("produto-destaque1");
+  produto_destaque1.insertAdjacentHTML("afterbegin", produtoDestaque.mostrarProdutosEmDestaque());
 
-const produto = new Produto("Guitarra", "20-02-2023", "Guitarra pop preta pintura brilhante ultra moderna", 1350.99)
-const produtoDestaque = new ProdutoDestaque("Estrela Ninja", "18-07-2013","Estrela ninja preta brilhante alcance longo fatal", 809.99, "shuriken.jpg", "ESTRELA NINJA" )
+  const produto_destaque2 = document.getElementById("produto-destaque2");
+  produto_destaque2.insertAdjacentHTML("afterbegin", produtoDestaque.mostrarProdutosEmDestaque());
+  
+  const produto = new Produto(
+    "Guitarra",
+    "20-02-2023",
+    "Guitarra pop preta pintura brilhante ultra moderna",
+    "R$ 1350.99"
+  );
+  const produtO = document.getElementById
+  
 
-const produto_destaque = document.getElementById("produto-destaque")
-produto_destaque.insertAdjacentHTML('afterbegin', produtoDestaque.mostrarProdutosEmDestaque());
+// Obtém o seletor de cores
+const colorPicker = document.getElementById('color-picker');
 
-console.log(produto.mostrarDados())
-console.log(produtoDestaque.mostrarProdutosEmDestaque())
+// Adiciona um evento de mudança ao seletor de cores
+colorPicker.addEventListener('change', function() {
+  // Obtém o valor selecionado
+  const color = colorPicker.value;
+
+  // Define a cor de fundo do body
+  document.body.style.backgroundColor = color;
+});
+
